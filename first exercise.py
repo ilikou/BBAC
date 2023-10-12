820d7019e41a8054532e8b2d846263c4b7ac8246
Koutroumanos Ilias <ilikou@DeltaFoods.gr>
2023-10-12 21:12:02 +0300

    first exercise

---
+++ first exercise.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=5
+b=225
+c=a+b
+print(c)
+
+
+# In[2]:
+
+
+7/3
+
+
+# In[3]:
+
+
+7//3
+
+
+# In[4]:
+
+
+s='There is a nice bar on the island!' 
+t='to relax!' 
+final=s+t
+final
+
+
+# In[5]:
+
+
+final.find('is')
+
+
+# In[6]:
+
+
+final.find('is',7)
+
+
+# In[7]:
+
+
+final.index('bar')
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+final[16:19]
+
+
+# In[9]:
+
+
+final.count('',0,18)
+
+
+# In[10]:
+
+
+final.replace('nice','') .replace('  ',' ')
+
+
+# In[11]:
+
+
+s[::-1]
+
+
+# In[12]:
+
+
+name='Data Scientists'
+perce=80
+finalper='9.999%'
+print(f'{name} spend {perce} % of their time working with data and only {finalper} working with Machine Learning!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
